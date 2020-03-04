c7c50172513cbf352c29d9bfffa49c4d0b637540
cuong292 <cuongnb@omt.com.vn>
2020-03-04 18:24:02 +0700

    hi

--- app/src/main/java/com/example/kotlinsample/data/ApiService.kt
+++ app/src/main/java/com/example/kotlinsample/data/ApiService.kt
@@ -5,6 +5,7 @@ import com.example.kotlinsample.BuildConfig
 import com.example.kotlinsample.data.interceptor.HeaderInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class ApiService {
--- app/src/main/java/com/example/kotlinsample/data/AppDBHelper.kt
+++ app/src/main/java/com/example/kotlinsample/data/AppDBHelper.kt
@@ -3,10 +3,26 @@ package com.example.kotlinsample.data
 import android.content.Context
 import com.example.kotlinsample.base.DatabaseName
 import com.example.kotlinsample.data.dataInterface.DatabaseHelper
+import io.realm.RealmConfiguration
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AppDBHelper @Inject constructor(val context: Context, @DatabaseName val dbName: String) :
+class AppDBHelper @Inject constructor(val context: Context, @DatabaseName var dbName: String) :
     DatabaseHelper {
+
+    fun reConfigName(realmDb: String) {
+        dbName = realmDb;
+    }
+
+    companion object RealmInstance {
+        private lateinit var realmConfig: RealmConfiguration
+        fun getInstance() {
+            realmConfig = RealmConfiguration.Builder()
+//                .name(dbName)
+                .schemaVersion(3)
+                .build()
+        }
+    }
+
 }
